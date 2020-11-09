ea25a5bf4f16ea94eaba9771d8e81d4dd6eca4e7
andresguisado <andresguisado1@gmail.com>
2020-11-09 13:38:39 +0000

    Adding install docker script

---
+++ install_docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## Installing curl and jq
+
+sudo yum install epel-release -y
+sudo yum install curl -y
+sudo yum install jq -y
+
+
+## Installing docker
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+#sudo usermod -aG docker centos
+sudo rm get-docker.sh
+sudo systemctl enable docker
+sudo systemctl start docker
+
+## Installing docker-compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
