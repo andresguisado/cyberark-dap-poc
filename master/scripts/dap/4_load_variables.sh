a6f0751aafc37c21371922f847de75223c2ac493
andresguisado <andresguisado1@gmail.com>
2020-11-09 19:48:42 +0000

    Fixing path

--- master/scripts/dap/4_load_variables.sh
+++ master/scripts/dap/4_load_variables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export targetsshkey=$(cat ../config/ansible/target_priv_key)
+export targetsshkey=$(cat config/ansible/target_priv_key)
 docker exec -it dap-cli -e targetsshkey=$targetsshkey bash -c "sh load_variables.sh"
 
