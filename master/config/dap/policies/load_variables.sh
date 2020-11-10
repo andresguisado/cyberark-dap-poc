b36ac44f020571d274328d69e582d97af7d681e5
andresguisado <andresguisado1@gmail.com>
2020-11-10 10:23:13 +0000

    Fixing dummy variables

--- master/config/dap/policies/load_variables.sh
+++ master/config/dap/policies/load_variables.sh
@@ -2,16 +2,23 @@
 
 #Secrets
 
+# /dev/cicd/ansible
+conjur variable values add dev/cicd/ansible/access_key_id  XXXXXX
+conjur variable values add dev/cicd/ansible/access_key_secret YYYYY
+conjur variable values add dev/cicd/ansible/targetuser root
+conjur variable values add dev/cicd/ansible/targetpwd Cyberark1
+conjur variable values add dev/cicd/ansible/targetsshkey test
+
 # /dev/cicd/jenkins
 conjur variable values add dev/cicd/jenkins/access_key_id  XXXXXX
 conjur variable values add dev/cicd/jenkins/access_key_secret YYYYY
 conjur variable values add dev/cicd/jenkins/targetuser root
 conjur variable values add dev/cicd/jenkins/targetpwd Cyberark1
-conjur variable values add dev/cicd/jenkins/targetsshkey $targetsshkey
+conjur variable values add dev/cicd/jenkins/targetsshkey test
 
 # /dev/cicd/azuredevops
 conjur variable values add dev/cicd/azuredevops/access_key_id  XXXXXX
 conjur variable values add dev/cicd/azuredevops/access_key_secret YYYYY
 conjur variable values add dev/cicd/azuredevops/targetuser root
 conjur variable values add dev/cicd/azuredevops/targetpwd Cyberark1
-conjur variable values add dev/cicd/azuredevops/targetsshkey $targetsshkey
+conjur variable values add dev/cicd/azuredevops/targetsshkey test
--- master/scripts/dap/4_load_variables.sh
+++ master/scripts/dap/4_load_variables.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-export targetsshkey=$(cat config/ansible-target/target_priv_key)
-docker exec -it dap-cli -e targetsshkey=$targetsshkey bash -c "sh load_variables.sh"
+
+#export targetsshkey=$(cat config/ansible-target/target_priv_key)
+#docker exec -it dap-cli -e targetsshkey=$targetsshkey bash -c "sh load_variables.sh"
+## It seems above it doesn't work
+
+docker exec -it dap-cli bash -c "sh load_variables.sh"
 
