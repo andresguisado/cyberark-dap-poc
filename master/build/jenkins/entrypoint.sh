6b41b5de713fdcd0b9bae848566d7ee97c68e0ee
andresguisado <andresguisado1@gmail.com>
2020-11-10 13:31:36 +0000

    Amends jenkins-conjur-ansible image

--- master/build/jenkins/entrypoint.sh
+++ master/build/jenkins/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 #/usr/local/bin/plugins.sh /usr/share/jenkins/plugins.txt
-#yum install -y ansible
-#RUN mkdir -p $JENKINS_HOME/.ansible/collections
-ansible-galaxy collection install /tmp/cyberark-conjur-1.0.5.tar.gz -p $JENKINS_HOME/.ansible/collections
+#ansible-galaxy collection install /tmp/cyberark-conjur-1.0.7.tar.gz -p $JENKINS_HOME/.ansible/collections
+ansible-galaxy collection install cyberark.conjur
 tail -f /dev/null
-#ansible-galaxy collection install cyberark.conjur
