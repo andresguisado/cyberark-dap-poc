00ecdcab3adbba9c3a7d2d42380a395b8ae79b3f
Andres Guisado <andresguisado@users.noreply.github.com>
2025-04-12 19:51:31 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -18,5 +18,6 @@ sudo systemctl start docker
 
 ## Installing docker-compose
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
